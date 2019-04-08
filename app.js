7c0d24c17652b29a959b30d28420eb36f70ab0df
Gustav <frohlich.gfx@gmail.com>
2019-04-08 23:31:00 +0200

    implemented some features, like active player indicator, change player if 1, add up the score

--- app.js
+++ app.js
@@ -5,12 +5,33 @@ activePlayer = 0;
 
 document.querySelector('.dice').style.display = 'none'; 
 
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
+
 document.querySelector('.btn-roll').addEventListener('click', function() {
     var dice = Math.floor(Math.random() * 6) + 1;
     var diceDOM = document.querySelector('.dice');
     diceDOM.style.display = 'block';
     diceDOM.src = 'dice-' + dice + '.png'; 
     
+    if (dice !== 1){
+        roundscores += dice;
+        document.querySelector('#current-' + activePlayer).textContent = roundscores;
+    } else {
+        activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+        roundscores = 0;
+        
+        document.getElementById('current-0').textContent = '0';
+        document.getElementById('current-1').textContent = '0';
+    
+        document.querySelector('.player-0-panel').classList.toggle('active'); 
+        document.querySelector('.player-1-panel').classList.toggle('active'); 
+        
+        document.querySelector('.dice').style.display = 'none';
+        
+    }
 });
 
 
