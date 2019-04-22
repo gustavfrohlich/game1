54a7f4f81190725f932690da6be39eb13c073e17
gustav <gustav@Gustavs-iMac.local>
2019-04-22 22:15:55 +0200

    small feature: the user is now able to set the winning score, the default is 100

--- app.js
+++ app.js
@@ -32,7 +32,15 @@ document.querySelector('.btn-hold').addEventListener('click', function(){
 
     document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
 
-    if (scores[activePlayer] >= 100){
+    var input = document.querySelector('.final-score').value;
+    var winningScore;
+    if(input){
+        winningScore = input;
+    }else{
+        winningScore = 100;
+    }
+
+    if (scores[activePlayer] >= winningScore){
         gamePlaying = false;
         document.getElementById('name-' + activePlayer).textContent = 'Winner!';
         document.querySelector('.dice').style.display = 'none';
