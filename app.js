ef31f91a3145b5b086a366bcd76d67398949f582
gustav <gustav@Gustavs-iMac.local>
2019-04-22 17:57:55 +0200

    added the functionality of Winning if player hits 100 points, moreover tried to clean the code so theres no unnecessary repeating

--- app.js
+++ app.js
@@ -14,29 +14,44 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
     var dice = Math.floor(Math.random() * 6) + 1;
     var diceDOM = document.querySelector('.dice');
     diceDOM.style.display = 'block';
-    diceDOM.src = 'dice-' + dice + '.png'; 
+    diceDOM.src = 'dice-' + dice + '.png';   
     
     if (dice !== 1){
         roundscores += dice;
         document.querySelector('#current-' + activePlayer).textContent = roundscores;
     } else {
-        activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
-        roundscores = 0;
-        
-        document.getElementById('current-0').textContent = '0';
-        document.getElementById('current-1').textContent = '0';
-    
-        document.querySelector('.player-0-panel').classList.toggle('active'); 
-        document.querySelector('.player-1-panel').classList.toggle('active'); 
-        
-        document.querySelector('.dice').style.display = 'none';
+        nextPlayer();
         
     }
 });
 
+document.querySelector('.btn-hold').addEventListener('click', function(){
+    scores[activePlayer] += roundscores;
 
+    document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
 
+    if (scores[activePlayer] >= 20){
+        document.getElementById('name-' + activePlayer).textContent = 'Winner!';
+    } else {
 
+    }
+
+    nextPlayer();
+
+});
+
+function nextPlayer(){
+    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+    roundscores = 0;
+        
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('current-1').textContent = '0';
+    
+    document.querySelector('.player-0-panel').classList.toggle('active'); 
+    document.querySelector('.player-1-panel').classList.toggle('active'); 
+        
+    document.querySelector('.dice').style.display = 'none';
+}
 
 
 
